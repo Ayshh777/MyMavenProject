7e0b61d1060bbe1a28847d0510652a1b807043cc
Ayshh777 <shaikayeshamathar@gmail.com>
2026-09-17 12:46:59 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,10 @@
+package java;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("This is Sample");
+		System.out.println(" Aysha");
+		System.out.println("Age 17");
+	}
+}
