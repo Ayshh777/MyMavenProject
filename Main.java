0730b2e6cf6555ef28db744982a40fea9984e6e3
Ayshh777 <shaikayeshamathar@gmail.com>
2026-09-23 11:16:33 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,14 +5,14 @@ import com.portfolioproject.app.model.Stock;
 import com.portfolioproject.app.model.MutualFund;
 import com.portfolioproject.app.model.Holding;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
 
     // Store all users
-    static List<User> users = new ArrayList<>();
+    static Map<String, User> users = new HashMap<>();
 
     public static void main(String[] args) {
 
@@ -60,8 +60,8 @@ public class Main {
                     User newUser = new User(userid, name, email);
 
                     // Add user to users list
-                    users.add(newUser);
-
+                    users.put(userid, newUser); //userid, name, email);
+                    users.put(userid,  newUser); //userid User object
                     System.out.println("User created successfully!");
 
                     break;
@@ -208,10 +208,9 @@ public class Main {
 
                         System.out.println("\n--- User Details ---");
 
-                        for (User user : users) {
-
+                        for (User user : users.values())
+                        {
                             user.display();
-
                             System.out.println("----------------------------");
                         }
                     }
@@ -233,7 +232,8 @@ public class Main {
 
                     System.out.println("\n--- Holdings ---");
 
-                    for (User user : users) {
+                       for (User user : users.values())
+                       {
 
                         System.out.println("\nUser ID: " + user.getUserid());
                         System.out.println("User Name: " + user.getName());
@@ -284,16 +284,8 @@ public class Main {
     // =========================================
     // FIND USER BY USER ID
     // =========================================
-    public static User findUser(String userid) {
-
-        for (User user : users) {
-
-            if (user.getUserid().equals(userid)) {
-
-                return user;
-            }
-        }
-
-        return null;
-    }
-}
+    public  static User findUser(String userid)
+    {
+    	return users.get(userid);
+    }	
+  }
